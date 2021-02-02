fde2d9b7070229bc9e49060ca4c29db62a540aaf
georgie123 <georges.hinot@gmail.com>
2021-02-02 19:11:22 +0100

    Some tests

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
 from datetime import date
-
-import pandas as pd
-from tabulate import tabulate as tab
-
-from main import (df)
+import openpyxl
+from openpyxl.utils import get_column_letter
 
 today = date.today()
 
+outputExcelFile = r'C:/Users/Georges/Downloads/'+str(today)+' Stats Hivebrite.xlsx'
+workbook = openpyxl.load_workbook(outputExcelFile)
+sheetsLits = workbook.sheetnames
 
 
-print(tab(df_Industries_count.head(30), headers='keys', tablefmt='psql', showindex=False))
+# EXCEL COLUMN SIZE
+for sheet in sheetsLits:
+    for cell in workbook[sheet][1]:
+        if get_column_letter(cell.column) == 'A':
+            workbook[sheet].column_dimensions[get_column_letter(cell.column)].width = 30
+        else:
+            workbook[sheet].column_dimensions[get_column_letter(cell.column)].width = 10
+
+        workbook.save(outputExcelFile)
+
+
+
+# ws.column_dimensions['A'].width = 75
+# wb.save("C:/Users/Georges/Downloads/2021-02-02 Stats Hivebrite.xlsx")
